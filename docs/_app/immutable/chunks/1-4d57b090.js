48752f42bb834315b78d8b7bafe64a767672d967
Doug A <douganderson444@gmail.com>
2022-12-06 13:32:29 -0400

    gh pages build

--- docs/_app/immutable/chunks/1-4d57b090.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-4b41a95b.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-41c22d8e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-c9dbc798.js";export{t as component};
