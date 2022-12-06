fb0daa161b583b943c048c9bb15c5b02d4d2bde0
Doug A <douganderson444@gmail.com>
2022-12-06 15:12:34 -0400

    copy nodeEl fontFamily & append it

--- src/lib/fillwidth.js
+++ src/lib/fillwidth.js
@@ -2,12 +2,30 @@ import FitTest from './FitTest.svelte';
 
 // 	Inspired by: https://stackoverflow.com/questions/30940499/text-size-auto-resizing-on-div-width
 
-export function fillWidth(nodeEl, { width = 227, fontSize = 20 } = {}) {
+export function fillWidth(nodeEl, { width = 227 } = {}) {
 	if (!nodeEl.innerHTML) return;
+
 	let fitTest;
+	let fontSize = 10;
+
+	// set white-space: nowrap on nodeEl
+	// to prevent line breaks from affecting the width
+	nodeEl.style.whiteSpace = 'nowrap';
+
+	// get the closest  fontSize inherited from the parent
+	const copyEl = document.createElement(nodeEl.nodeName.toLowerCase());
+	// set copyEl.style same aas nodeEl.style
+	copyEl.style.fontFamily = window.getComputedStyle(nodeEl).fontFamily;
+	copyEl.style.fontSize = fontSize;
+	copyEl.style.opacity = 0;
+	copyEl.style.position = 'absolute';
+	copyEl.style.display = 'hidden';
+	copyEl.style.whiteSpace = 'nowrap';
+
+	document.body.appendChild(copyEl);
 
 	fitTest = new FitTest({
-		target: document.body,
+		target: copyEl,
 		props: {
 			text: nodeEl.innerHTML,
 			fontSize
